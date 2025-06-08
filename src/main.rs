627e26d13b4d5c1aef9a94a07021b45bf35540d3
rtthw <172864013+rtthw@users.noreply.github.com>
2025-06-08 08:44:48 -0400

    Create area type

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 
 
 
-use std::any::Any;
+use std::{any::{Any, TypeId}, collections::HashMap};
 
 
 
@@ -18,6 +18,19 @@ pub trait Object: 'static {
 
     fn new(state: Self::State) -> Self;
     fn update(&mut self, state: Self::State) -> bool;
+    fn resize(&mut self, area: Rect) -> bool;
+}
+
+// TODO: Separate IDs for objects of the same type.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct Id(TypeId);
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Rect {
+    pub x: f32,
+    pub y: f32,
+    pub w: f32,
+    pub h: f32,
 }
 
 
@@ -28,6 +41,7 @@ pub trait Object: 'static {
 
 trait DummyObject {
     fn update(&mut self, state: Box<dyn Any>) -> bool;
+    fn resize(&mut self, area: Rect) -> bool;
 }
 
 impl<T: Object> DummyObject for T {
@@ -38,10 +52,16 @@ impl<T: Object> DummyObject for T {
             *state.downcast().expect("incorrect object state type when downcasting"),
         )
     }
+
+    #[inline]
+    fn resize(&mut self, area: Rect) -> bool {
+        <Self as Object>::resize(self, area)
+    }
 }
 
 trait ObjectTemplate {
     fn create(&mut self) -> Box<dyn DummyObject + 'static>;
+    fn id(&self) -> Id;
     fn state(&mut self) -> Box<dyn Any>;
 }
 
@@ -58,10 +78,17 @@ impl<T: Object> ObjectDef<T> {
 }
 
 impl<T: Object> ObjectTemplate for ObjectDef<T> {
+    #[inline]
     fn create(&mut self) -> Box<dyn DummyObject + 'static> {
         Box::new(T::new(self.take_state()))
     }
 
+    #[inline]
+    fn id(&self) -> Id {
+        Id(TypeId::of::<T>())
+    }
+
+    #[inline]
     fn state(&mut self) -> Box<dyn Any> {
         Box::new(self.take_state())
     }
@@ -75,14 +102,38 @@ impl<T: Object> ObjectTemplate for ObjectDef<T> {
 
 pub struct Tree {
     root: Node,
+    objects: HashMap<Id, TreeObject>,
 }
 
 impl Tree {
-    pub fn update(&mut self) {
-        self.root.crawl(&mut |object_template| {
-            todo!()
+    pub fn update(&mut self, area: Rect) {
+        let Tree { root, objects } = self;
+
+        root.crawl(area, &mut | ObjectNode { template, area } | {
+            let id = template.id();
+            let mut newly_created = false;
+            let tree_object = objects.entry(id).or_insert_with(|| {
+                let dummy = template.create();
+                newly_created = true;
+                TreeObject {
+                    dummy,
+                    area,
+                }
+            });
+
+            if !newly_created {
+                let mut changed = tree_object.dummy.update(template.state());
+                if area != tree_object.area {
+                    changed = tree_object.dummy.resize(area) || changed;
+                }
+            }
         });
     }
+}
+
+struct TreeObject {
+    dummy: Box<dyn DummyObject>,
+    area: Rect,
 }
 
 enum Node {
@@ -91,19 +142,25 @@ enum Node {
 }
 
 impl Node {
-    fn crawl(
-        &mut self,
-        func: &mut impl FnMut(&mut Box<dyn ObjectTemplate>),
-    ) {
+    fn crawl(&mut self, area: Rect, func: &mut impl FnMut(ObjectNode)) {
         match self {
             Self::Container(nodes) => {
                 for child in nodes.iter_mut() {
-                    child.crawl(func);
+                    // TODO: Do layout here.
+                    child.crawl(area, func);
                 }
             }
             Self::Object(template) => {
-                func(template);
+                func(ObjectNode {
+                    template,
+                    area,
+                });
             }
         }
     }
+}
+
+struct ObjectNode<'a> {
+    template: &'a mut Box<dyn ObjectTemplate>,
+    area: Rect,
 }
