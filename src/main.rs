bee2d19f5a15d047c3b3b3cc8c0bc6fe84f5f06c
rtthw <172864013+rtthw@users.noreply.github.com>
2025-06-11 12:15:48 -0400

    Continue new object tree implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::marker::PhantomData;
 
+
+
+use std::{any::Any, marker::PhantomData};
 
 
 
@@ -18,28 +20,9 @@ pub trait Process {
     type Input;
     type Output;
 
-    fn begin(&mut self, args: Self::Args) {}
+    fn begin(args: Self::Args) -> Self;
     fn process(&mut self, input: Self::Input) -> Self::Output;
     fn end(&mut self) {}
-}
-
-
-pub struct Renderer<'pass> {
-    _pass: &'pass mut PhantomData<()>,
-}
-
-pub trait Render {
-    fn render(&mut self, renderer: &mut Renderer);
-}
-
-impl<'pass> Process for Renderer<'pass> {
-    type Args = ();
-    type Input = &'pass mut dyn Render;
-    type Output = ();
-
-    fn process(&mut self, input: Self::Input) -> Self::Output {
-        input.render(self);
-    }
 }
 
 
@@ -49,3 +32,97 @@ impl<'pass> Process for Renderer<'pass> {
 
 
 // --- Implementation
+
+
+
+pub struct RenderPass<'pass> {
+    _pass: &'pass mut PhantomData<()>,
+}
+
+pub trait Render {
+    fn render(&mut self, pass: &mut RenderPass);
+}
+
+impl Render for Box<dyn Render> {
+    fn render(&mut self, pass: &mut RenderPass) {
+        self.as_mut().render(pass);
+    }
+}
+
+impl<'pass> Process for RenderPass<'pass> {
+    type Args = &'pass mut PhantomData<()>;
+    type Input = &'pass mut dyn Render;
+    type Output = ();
+
+    fn begin(args: Self::Args) -> Self {
+        Self {
+            _pass: args,
+        }
+    }
+
+    fn process(&mut self, input: Self::Input) -> Self::Output {
+        input.render(self);
+    }
+}
+
+
+
+pub struct UpdatePass<'c, C> {
+    context: &'c mut C,
+}
+
+pub trait Update {
+    type Context;
+
+    fn update(&mut self, context: &mut Self::Context);
+}
+
+impl<C> Update for Box<dyn Update<Context = C>> {
+    type Context = C;
+
+    fn update(&mut self, context: &mut Self::Context) {
+        self.as_mut().update(context);
+    }
+}
+
+impl<'c, C> Process for UpdatePass<'c, C> {
+    type Args = &'c mut C;
+    type Input = &'c mut dyn Update<Context = C>;
+    type Output = ();
+
+    fn begin(args: Self::Args) -> Self {
+        Self {
+            context: args,
+        }
+    }
+
+    fn process(&mut self, input: Self::Input) -> Self::Output {
+        input.update(self.context)
+    }
+}
+
+
+
+struct Tree {
+    objects: Vec<Box<dyn Any>>,
+}
+
+impl Tree {
+    fn render<'pass>(&'pass mut self, pass: &'pass mut RenderPass<'pass>) {
+        for renderable in self.objects
+            .iter_mut()
+            .filter_map(|o| o.downcast_mut::<Box<dyn Render>>())
+        {
+            pass.process(renderable)
+        }
+    }
+
+    fn update<'pass, C: 'static>(&'pass mut self, pass: &'pass mut UpdatePass<'pass, C>) {
+        for updateable in self.objects
+            .iter_mut()
+            .filter_map(|o| o.downcast_mut::<Box<dyn Update<Context = C>>>())
+        {
+            pass.process(updateable)
+        }
+    }
+}
