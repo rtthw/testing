9f3ee9dfede1e70a21c8fad50814e5baf6e1b98a
rtthw <172864013+rtthw@users.noreply.github.com>
2025-06-20 09:15:47 -0400

    Move function dropper to separate example

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@
 
 
 fn main() {
-    let mut something = Something;
-    do_something(&mut something);
 }
 
 
@@ -12,37 +10,8 @@ fn main() {
 
 
 
-pub trait Renderable {
-    fn render(&mut self);
-}
-
-pub struct Render<'a, T: Renderable>(&'a mut T);
-
-impl<'a, T: Renderable> Drop for Render<'a, T> {
-    fn drop(&mut self) {
-        println!("Rendering...");
-        self.0.render();
-    }
-}
-
-
-
 // --- Utility types
 
 
 
 // --- Implementation
-
-
-
-struct Something;
-
-impl Renderable for Something {
-    fn render(&mut self) {
-        println!("Rendering something...");
-    }
-}
-
-fn do_something(something: &mut Something) {
-    Render(something);
-}
