f048aeba73c6dd57f2029dcfe28980f521a2f804
rtthw <172864013+rtthw@users.noreply.github.com>
2025-06-30 09:38:46 -0400

    Create simple sizing resolver

--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,7 @@ impl Tree {
             parents: &mut SecondaryMap<Id, Option<Id>>,
         ) {
             let id = nodes.insert(NodeInfo {
+                area: Rect::NONE, // TODO
                 style: node.style,
             });
             let _ = parents.insert(id, Some(parent));
@@ -127,17 +128,117 @@ impl Style {
         self.axis = Axis::Vertical;
         self
     }
+
+    pub fn auto_sized(mut self) -> Self {
+        self.sizing = Sizing::Auto;
+        self
+    }
+
+    pub fn exact_sized(mut self, length: f32) -> Self {
+        self.sizing = Sizing::Exact(length);
+        self
+    }
+
+    pub fn portion_sized(mut self, portion: f32) -> Self {
+        self.sizing = Sizing::Portion(portion);
+        self
+    }
 }
 
 
 
+#[derive(Clone)]
 pub enum Sizing {
     Auto,
-    Exact(Vec2),
+    Exact(f32),
     Portion(f32),
 }
+
+impl Sizing {
+    pub fn is_auto(&self) -> bool {
+        matches!(self, Sizing::Auto)
+    }
+
+    pub fn as_exact(&self) -> Option<f32> {
+        match self {
+            Sizing::Exact(n) => Some(*n),
+            _ => None,
+        }
+    }
+
+    pub fn as_portion(&self) -> Option<f32> {
+        match self {
+            Sizing::Portion(n) => Some(*n),
+            _ => None,
+        }
+    }
+}
+
+
+fn resolve_sizes(length: f32, sizings: Vec<Sizing>) -> Vec<f32> {
+    let mut sizes = [length / sizings.len() as f32].repeat(sizings.len());
+
+    let auto_count: usize = sizings.iter()
+        .fold(0, |acc, s| if s.is_auto() { acc + 1 } else { acc });
+    let mut remaining = length;
+
+    for (i, exact) in sizings.iter()
+        .enumerate()
+        .filter_map(|(i, s)| Some((i, s.as_exact()?)))
+    {
+        sizes[i] = exact;
+        remaining -= exact;
+    }
+
+    for (i, portion) in sizings.iter()
+        .enumerate()
+        .filter_map(|(i, s)| Some((i, s.as_portion()?)))
+    {
+        let size = length * portion;
+        sizes[i] = size;
+        remaining -= size;
+    }
+
+    let auto_size = remaining / auto_count as f32;
+    for (i, _) in sizings.iter().enumerate().filter(|(_, s)| s.is_auto()) {
+        sizes[i] = auto_size;
+    }
+
+    sizes
+}
+
 
 pub enum Axis {
     Horizontal,
     Vertical,
+}
+
+
+#[cfg(test)]
+#[test]
+fn sizing_resolver_works() {
+    // NOTE: This is needed to account for floating point precision.
+    let round_sizes = |length: f32, sizings: &[Sizing]| -> Vec<f32> {
+        resolve_sizes(length, sizings.to_vec())
+            .into_iter()
+            .map(|s| (s * 10.0).round() / 10.0)
+            .collect()
+    };
+
+    assert_eq!(
+        round_sizes(12.0, &[Sizing::Auto, Sizing::Exact(4.0), Sizing::Portion(0.5)]),
+        vec![2.0, 4.0, 6.0],
+    );
+    assert_eq!(
+        round_sizes(12.0, &[Sizing::Auto, Sizing::Auto, Sizing::Portion(0.5)]),
+        vec![3.0, 3.0, 6.0],
+    );
+    assert_eq!(
+        round_sizes(12.0, &[Sizing::Auto, Sizing::Auto, Sizing::Auto]),
+        vec![4.0, 4.0, 4.0],
+    );
+    assert_eq!(
+        round_sizes(12.0, &[Sizing::Portion(0.4), Sizing::Portion(0.3), Sizing::Portion(0.2)]),
+        vec![4.8, 3.6, 2.4],
+    );
 }
