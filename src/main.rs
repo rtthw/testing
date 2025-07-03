0d4c240dbe1c724444d9c4ab744a547bfc3fb3ac
rtthw <172864013+rtthw@users.noreply.github.com>
2025-07-03 09:02:29 -0400

    Add structure object references

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,16 @@ use hecs::{Component as Member, ComponentError as MemberError, ComponentRef as M
 fn main() {
     let mut data = Data::new();
 
-    let root = Object::spawn(&mut data);
-    root.insert(&mut data, Something).unwrap();
+    let root = Object::new(&mut data);
+    {
+        let mut root_mut = root.as_mut(&mut data);
+        root_mut.insert(Something).unwrap();
+    }
+    {
+        let root_ref = root.as_ref(&data);
+        let _root_something = root_ref.get::<&Something>().unwrap();
+    }
 
-    let _root_something = root.get::<&Something>(&data).unwrap();
     println!("SUCCESS");
 }
 
@@ -35,19 +41,47 @@ impl Data {
 pub struct Object(Entity);
 
 impl Object {
-    pub fn spawn(data: &mut Data) -> Self {
+    pub fn new(data: &mut Data) -> Self {
         Self(data.world.spawn(()))
     }
 
-    pub fn get<'a, T: MemberRef<'a>>(&self, data: &'a Data) -> Result<T::Ref, MemberError> {
-        Ok(data.world
-            .entity(self.0)?
+    pub fn as_ref<'a>(&'a self, data: &'a Data) -> ObjectRef<'a> {
+        ObjectRef {
+            object: self,
+            data,
+        }
+    }
+
+    pub fn as_mut<'a>(&'a self, data: &'a mut Data) -> ObjectMut<'a> {
+        ObjectMut {
+            object: self,
+            data,
+        }
+    }
+}
+
+pub struct ObjectRef<'a> {
+    pub object: &'a Object,
+    pub data: &'a Data,
+}
+
+impl<'a> ObjectRef<'a> {
+    pub fn get<T: MemberRef<'a>>(&self) -> Result<T::Ref, MemberError> {
+        Ok(self.data.world
+            .entity(self.object.0)?
             .get::<T>()
             .ok_or_else(hecs::MissingComponent::new::<T::Component>)?)
     }
+}
 
-    pub fn insert(&self, data: &mut Data, member: impl Member) -> Result<(), hecs::NoSuchEntity> {
-        data.world.insert_one(self.0, member)
+pub struct ObjectMut<'a> {
+    pub object: &'a Object,
+    pub data: &'a mut Data,
+}
+
+impl<'a> ObjectMut<'a> {
+    pub fn insert(&mut self, member: impl Member) -> Result<(), hecs::NoSuchEntity> {
+        self.data.world.insert_one(self.object.0, member)
     }
 }
 
