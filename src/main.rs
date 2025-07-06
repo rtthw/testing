53220b1371a37cb17e9246a824fa068382329fc5
rtthw <172864013+rtthw@users.noreply.github.com>
2025-07-06 13:35:26 -0400

    Write out object definition macro

--- src/main.rs
+++ src/main.rs
@@ -2,3 +2,70 @@
 
 
 fn main() {}
+
+
+
+macro_rules! define {
+    () => {};
+
+    // pub Something;
+    (
+        $( #[$meta:meta] )*     // #[derive(...)]
+        $vis:vis $name:ident;   // pub Something;
+        $($tail:tt)*            // ...
+    ) => {
+        $( #[$meta] )*
+        $vis struct $name;
+        define!($($tail)*);
+    };
+
+    // pub Something(...);
+    (
+        $( #[$meta:meta] )*
+        $vis:vis $name:ident (
+            $(
+                $( #[$field_meta:meta] )*
+                $field_vis:vis $field_ty:ty
+            ),*
+        $(,)? );
+
+        $($tail:tt)*
+    ) => {
+        $( #[$meta] )*
+        $vis struct $name (
+            $(
+                $( #[$field_meta] )*
+                $field_vis $field_ty
+            ),*
+        );
+        define!($($tail)*);
+    };
+
+    // pub Something { field_a: u8, }
+    (
+        $( #[$meta:meta] )*
+        $vis:vis $name:ident {
+            $(
+                $( #[$field_meta:meta] )*
+                $field_vis:vis $field_name:ident : $field_ty:ty
+            ),*
+        $(,)? };
+        $($tail:tt)*
+    ) => {
+        $( #[$meta] )*
+        $vis struct $name {
+            $(
+                $( #[$field_meta] )*
+                $field_vis $field_name : $field_ty
+            ),*
+        }
+        define!($($tail)*);
+    };
+}
+
+define!{
+    Player;
+    pub Game {
+        player: Player,
+    };
+}
