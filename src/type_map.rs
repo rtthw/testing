1e86fd1ab4ad24948aef4f48f0a176ffcbc845cf
rtthw <172864013+rtthw@users.noreply.github.com>
2025-08-01 09:09:45 -0400

    Improve type map utility

--- src/type_map.rs
+++ src/type_map.rs
@@ -2,13 +2,13 @@
 
 
 
-use core::any::TypeId;
+use core::{any::TypeId, hash::{BuildHasherDefault, Hasher}};
 
 
 
 #[derive(Default)]
 pub struct TypeMap<V> {
-    map: rustc_hash::FxHashMap<TypeId, V>,
+    map: hashbrown::HashMap<TypeId, V, BuildHasherDefault<TypeIdHasher>>,
 }
 
 impl<V> TypeMap<V> {
@@ -30,5 +30,33 @@ impl<V> TypeMap<V> {
 
     pub fn clear(&mut self) {
         self.map.clear();
+    }
+}
+
+
+
+#[derive(Default)]
+pub struct TypeIdHasher {
+    hash: u64,
+}
+
+impl Hasher for TypeIdHasher {
+    fn write_u64(&mut self, n: u64) {
+        // Only a single value can be hashed, so the old hash should be zero.
+        debug_assert_eq!(self.hash, 0);
+        self.hash = n;
+    }
+
+    fn write_u128(&mut self, n: u128) {
+        debug_assert_eq!(self.hash, 0);
+        self.hash = n as u64;
+    }
+
+    fn write(&mut self, _bytes: &[u8]) {
+        panic!("Type ID is the wrong type!")
+    }
+
+    fn finish(&self) -> u64 {
+        self.hash
     }
 }
