258567792d48dde5dc2bfe1c028b6764c75fa35f
rtthw <172864013+rtthw@users.noreply.github.com>
2025-09-04 15:01:14 -0400

    Write up the latest dynamic dispatch idea

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,74 @@
 
 
 
-fn main() {}
+use std::{any::{Any, TypeId}, collections::HashMap, mem::transmute};
+
+
+
+fn main() {
+    struct Print;
+
+    struct A(u8);
+    struct B(&'static str);
+
+    impl Handle<Print> for A {
+        fn handle(&mut self, _data: Print) { println!("A({});", self.0); }
+    }
+    impl Handle<Print> for B {
+        fn handle(&mut self, _data: Print) { println!("B({});", self.0); }
+    }
+
+    let mut objects: Vec<Box<dyn Any>> = Vec::new();
+    objects.push(Box::new(A(1)));
+    objects.push(Box::new(B("Two")));
+
+    let mut callbacks = Callbacks { map: HashMap::default() };
+    callbacks.register(A::handle);
+    callbacks.register(B::handle);
+
+    for object in objects.iter_mut() {
+        assert!(callbacks.call_as_dyn(object, Print));
+    }
+}
+
+
+
+struct Callbacks {
+    map: HashMap<(TypeId, TypeId), usize>,
+}
+
+impl Callbacks {
+    fn register<T: 'static, U: 'static>(&mut self, call: fn(&mut T, U)) {
+        self.map.insert((TypeId::of::<T>(), TypeId::of::<U>()), call as usize);
+    }
+
+    fn call<T: 'static, U: 'static>(&self, data: &mut T, args: U) -> bool {
+        if let Some(ptr) = self.map.get(&(TypeId::of::<T>(), TypeId::of::<U>())) {
+            unsafe {
+                (transmute::<usize, fn(&mut T, U)>(*ptr))(data, args)
+            }
+            return true;
+        }
+
+        false
+    }
+
+    fn call_as_dyn<U: 'static>(&self, data: &mut Box<dyn Any>, args: U) -> bool {
+        let id = data.as_ref().type_id();
+        if let Some(ptr) = self.map.get(&(id, TypeId::of::<U>())) {
+            unsafe {
+                (transmute::<usize, fn(&mut dyn Handle<U>, U)>(*ptr))(
+                    transmute::<&mut dyn Any, &mut dyn Handle<U>>(data.as_mut()),
+                    args,
+                )
+            }
+            return true;
+        }
+
+        false
+    }
+}
+
+trait Handle<T> {
+    fn handle(&mut self, data: T);
+}
